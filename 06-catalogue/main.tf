d32d6b9982c6ec8c0bd745a180140c2e8089222b
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-09-12 13:35:34 +0530

    auto-scaling group and policy was added

--- 06-catalogue/main.tf
+++ 06-catalogue/main.tf
@@ -1,28 +1,28 @@
 resource "aws_lb_target_group" "catalogue" {
-  name     = "${local.name}-${var.tags.Component}"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = data.aws_ssm_parameter.vpc_id.value
+  name                 = "${local.name}-${var.tags.Component}"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = data.aws_ssm_parameter.vpc_id.value
   deregistration_delay = 60
   health_check {
-      healthy_threshold   = 2
-      interval            = 10
-      unhealthy_threshold = 3
-      timeout             = 5
-      path                = "/health"
-      port                = 8080
-      matcher = "200-299"
+    healthy_threshold   = 2
+    interval            = 10
+    unhealthy_threshold = 3
+    timeout             = 5
+    path                = "/health"
+    port                = 8080
+    matcher             = "200-299"
   }
 }
 
 module "catalogue" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
-  ami = data.aws_ami.centos8.id
+  ami                    = data.aws_ami.centos8.id
   name                   = "${local.name}-${var.tags.Component}-ami"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
   subnet_id              = element(split(",", data.aws_ssm_parameter.private_subnet_ids.value), 0)
-  iam_instance_profile = "ShellScriptRoleForRoboshop"
+  iam_instance_profile   = "ShellScriptRoleForRoboshop"
   tags = merge(
     var.common_tags,
     var.tags
@@ -38,9 +38,9 @@ resource "null_resource" "catalogue" {
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
-    host = module.catalogue.private_ip
-    type = "ssh"
-    user = "centos"
+    host     = module.catalogue.private_ip
+    type     = "ssh"
+    user     = "centos"
     password = "DevOps321"
   }
 
@@ -61,13 +61,13 @@ resource "null_resource" "catalogue" {
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = module.catalogue.id
   state       = "stopped"
-  depends_on = [ null_resource.catalogue ]
+  depends_on  = [null_resource.catalogue]
 }
 
 resource "aws_ami_from_instance" "catalogue" {
   name               = "${local.name}-${var.tags.Component}-${local.current_time}"
   source_instance_id = module.catalogue.id
-  depends_on = [ aws_ec2_instance_state.catalogue ]
+  depends_on         = [aws_ec2_instance_state.catalogue]
 }
 
 resource "null_resource" "catalogue_delete" {
@@ -81,16 +81,16 @@ resource "null_resource" "catalogue_delete" {
     command = "aws ec2 terminate-instances --instance-ids ${module.catalogue.id}"
   }
 
-  depends_on = [ aws_ami_from_instance.catalogue]
+  depends_on = [aws_ami_from_instance.catalogue]
 }
 
 resource "aws_launch_template" "catalogue" {
   name = "${local.name}-${var.tags.Component}"
 
-  image_id = aws_ami_from_instance.catalogue.id
+  image_id                             = aws_ami_from_instance.catalogue.id
   instance_initiated_shutdown_behavior = "terminate"
-  instance_type = "t2.micro"
-  update_default_version = true
+  instance_type                        = "t2.micro"
+  update_default_version               = true
 
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
 
@@ -102,4 +102,69 @@ resource "aws_launch_template" "catalogue" {
     }
   }
 
+}
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.name}-${var.tags.Component}"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 60
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  vpc_zone_identifier       = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  target_group_arns         = [aws_lb_target_group.catalogue.arn]
+
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = aws_launch_template.catalogue.latest_version
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+    #when there is new version of launch template, the auto scaling group should tigger automatically
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${local.name}-${var.tags.Component}"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+}
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.tags.Component}.app-${var.environment}.${var.zone_name}"]
+    }
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${local.name}-${var.tags.Component}"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 5.0
+  }
 }
