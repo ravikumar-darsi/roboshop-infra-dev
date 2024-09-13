46a32d5a9fd47f4b7d6e859f4ea61c485177456b
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-09-13 16:44:08 +0530

    querying vpc_id nd cart  Sg into aws_SSM

--- 13-cart/data.tf
+++ 13-cart/data.tf
@@ -10,6 +10,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
 
+
 data "aws_ssm_parameter" "app_alb_listener_arn" {
   name = "/${var.project_name}/${var.environment}/app_alb_listener_arn"
 }
